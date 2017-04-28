381a33b733aac4161fa97ee564a88e54776239c1
QLewis <qplewis@asu.edu>
2017-04-28 01:35:26 -0700

    restarting

--- Project_4/parser.h
+++ Project_4/parser.h
@@ -1,52 +1,21 @@
-//This parser is for project 4 and is based off the parser created by Rida Bazzi in Project 3
+//This parser is for project 4 and is based onff the parser creadted by Rida Bazzi in Project 3
 
-#ifndef _PARSER_H__
+#ifndef __PARSER_H__
 #define __PARSER_H__
 
 #include <string>
 #include "lexer.h"
 #include "compiler.h"
 
+
 class Parser{
-  private:
-	 LexicalAnalyzer lexer;
+	private:
+		LexicalAnalyzer lexer;
 
-	void syntax_error();
-	Token expect(TokenType expected_type);
-	Token peek();
+		void syntax_error();
+		Token expect(TokenType expected_type);
+		Token peek();
+	public:
+		//struct StatementNode* parse_generate_intermediate_representation();
 
-	struct StatementNode* parse_program();
-	struct StatementNode* parse_var_section();
-	struct StatementNode* parse_id_list();
-	struct StatementNode* parse_body();
-	struct StatementNode* parse_stmt_list();
-	struct StatementNode* parse_stmt();
-	struct StatementNode* parse_assign_stmt();
-	struct StatementNode* parse_expr();
-	struct StatementNode* parse_primary();
-	struct StatementNode* parse_op();
-	struct StatementNode* parse_print_stmt();
-	struct StatementNode* parse_while_stmt();
-	struct StatementNode* parse_if_stmt();
-	struct StatementNode* parse_condition();
-	struct StatementNode* parse_relop();
-	struct StatementNode* parse_switch_stmt();
-	struct StatementNode* parse_for_stmt();
-	struct StatementNode* parse_case_list();
-	struct StatementNode* parse_case();
-	struct StatementNode* parse_default_case();
-
- public:
-	//void ParseInput();	
-	struct StatementNode* parse_generate_intermediate_representation();
 };
-
-struct varList
-{
-	struct ValueNode* value;
-	struct ValueNode* next;
-}
-
-#endif
-
-
